4920663074d3eefacab9e167623a037c13403ede
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-26 10:27:40 +0700

    mengecek apakah antrian kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -28,3 +28,16 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            
+        }
+    }
+}
