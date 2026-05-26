91ee9bb29e80f686a80c80dd9f25325a688bbe29
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-26 10:30:24 +0700

    jika rear berada di posisi terakhir array, kembali ke awal array

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -22,14 +22,14 @@ public:
         cin >> num;
         cout << endl;
 
-        //cek apakah antrian penuh
+        // cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
             cout << "\nQueue overflow\n";
             return;
         }
 
-        //cek apakah antrian kosong
+        // cek apakah antrian kosong
         if (FRONT == -1)
         {
             FRONT = 0;
@@ -37,7 +37,14 @@ public:
         }
         else
         {
-            
+            // jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
         }
+        queue_array[REAR] = num;
     }
+
+    
 }
