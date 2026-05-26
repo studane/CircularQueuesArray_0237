ede4e07be0f57c9996fdc0af3536a93de0cde8ea
studane <zhidaneprabowoguzlend@gmail.com>
2026-05-26 10:40:28 +0700

    cek apakah antrian kosong

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -77,5 +77,14 @@ public:
 
         int FRONT_Position = FRONT;
         int REAR_Position = REAR;
+
+        // cek apakah antrian kosong
+        if (FRONT_Position == -1)
+        { 
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queue are....\n";
     }
 }
